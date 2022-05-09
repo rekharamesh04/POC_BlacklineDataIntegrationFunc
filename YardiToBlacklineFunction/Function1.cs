b88a2e9314cb4c0c888f3e271be933f8acc53463
Rekha Ramesh <RRamesh@linklogistics.com>
2022-05-09 14:29:26 -0500

    changes to use az db

--- YardiToBlacklineFunction/Function1.cs
+++ YardiToBlacklineFunction/Function1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Net;
@@ -9,88 +10,133 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
 
 namespace YardiToBlacklineFunction
 {
     public class Function1
     {
-        [FunctionName("Function1")]
-        public async Task RunAsync([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log)
+        [FunctionName ( "Function1" )]
+        public async Task RunAsync ( [TimerTrigger ( "0 */1 * * * *" )] TimerInfo myTimer, ILogger log )
         {
             // [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestMessage req,
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation ( $"C# Timer trigger function executed at: {DateTime.Now}" );
+            //call Blackline and get the token           
+            Token token = new Token ();
+            var tokenurl = Environment.GetEnvironmentVariable ( "blacklineUrlBase" ) + "connect/token";
+            HttpClient newClient = new HttpClient ();
 
+            var data = new[]
+                        {
+                            new KeyValuePair<string, string>("grant_type", "password"),
+                            new KeyValuePair<string, string>("password", "f687e84fa6ee5856203bccda73262fe2"),
+                            new KeyValuePair<string, string>("username", "onecloud"),
+                            new KeyValuePair<string, string>("scope", "DataIngestionAPI instance_6E92F34B-C06F-49ED-AF71-CA906EA1755B"),
+                            new KeyValuePair<string, string>("client_secret", "&[pro~)=tn4e"),
+                            new KeyValuePair<string, string>("client_id", "linklogistics_sts2")
+                        };
+            //Read Server Response
+            newClient.DefaultRequestHeaders.TryAddWithoutValidation ( "Content-Type", "application/octet-stream" );
+            HttpResponseMessage tokenresponse = await newClient
+                .PostAsync ( string.Format ( tokenurl ), new FormUrlEncodedContent ( data ) );
+            if (tokenresponse.StatusCode == HttpStatusCode.OK)
+            {
+                var jsonContent = await tokenresponse.Content.ReadAsStringAsync ();
+                token = JsonConvert.DeserializeObject<Token> ( jsonContent );
+
+            }
+            else
+            {
+                log.LogInformation ( $"Function is unable to get Blackline token at: {DateTime.Now}" );
+                return;
+            }
             //Call yardi get data
             // Get the connection string from app settings and use it to create a connection.
-            var str = Environment.GetEnvironmentVariable("yardisql_connection");
-            DataTable dt = new DataTable();
-            using (SqlConnection conn = new SqlConnection(str))
+            var str = Environment.GetEnvironmentVariable ( "IntegrationDBsql_connection" );
+            DataTable dt = new DataTable ();
+            using (SqlConnection conn = new SqlConnection ( str ))
             {
-                conn.Open();
-                var text = "select top 1 'PRO-C' + char(9) + RTRIM(a.SCODE) + char(9) + '' + RTRIM(p.HPROP) + char(9) + '1102' + char(9) + 'BK' + char(9) + '12111' + char(9) + '' + char(9) + '' + char(9) + '' + char(9) + a.SDESC + char(9) + b.BookName + char(9) + 'A' + char(9) + STR(a.IACCTTYPE) "+
-    " + char(9) + 'TRUE' + char(9) + 'TRUE' + char(9) + 'GBP' + char(9) + 'USD' + char(9) + '6/30/2022' + char(9) + '34664.86' + char(9) + '23058.37' + char(9) + '34664.86' + char(9) + 'IMPCash 2' + char(9) + 'IMPCash 3' + char(9) + 'IMPCash 4' + char(9) + 'IMPCash 5' + char(9) + 'IMPCash 6' " +
+                conn.Open ();
+                var text = "  declare @selectPeriodDate Date = '2022-03-31' " +
+" select top 1 'PRO-C' + char(9) + AccountNumber + char(9) + PropertyID + char(9) " +
+"                               + Region + char(9) + Department + char(9)  " +
+"							   + Acquisition_Portfolio + char(9) + Sub_Region + char(9) + Cash_Entity + char(9) + Tenant_Code + char(9) + isnull(Book,'') + char(9) + AccountDescription  " +
+"							   + char(9) + 'cash' + char(9) + FinancialStatement	 + char(9) + 'Asset'  " +
+"                            + char(9) + ActiveAccount + char(9) + ActivityInPeriod + char(9) + 'GBP' + char(9) + AccountCurrency + char(9) + CONVERT(nvarchar,@selectPeriodDate) + char(9) +  " +
+"							GLAccountBalance + char(9) + '0' + char(9) + GLAccountBalance " +
+"		 + char(9) + 'IMPCash 2' + char(9) + 'IMPCash 3' + char(9) + 'IMPCash 4' + char(9) + 'IMPCash 5' + char(9) + 'IMPCash 6'  " +
+"         AS datarow, " +
+"		 CONVERT(varchar,AccountNumber)+ '|' + CONVERT(varchar,PropertyID) +'|'  " +
+"		 + CONVERT(varchar,@selectPeriodDate) +'|' + ISNULL(CONVERT(varchar,Book),0)  AS logid, " +
+"		 GLAccountBalance " +
+" from [dbo].[AccountsGL]";
 
-"from(Select HPROP " +
-
-      "from attributes " + 
-
-      "where SUBGROUP58 in ('Acquired', 'Marketing for Sale', 'Pending Sale', 'Held for Sale', 'Sold') "+
-
-        "AND SUBGROUP9 in ('Property', 'Development') "+
-
-        "AND SPROPNAME not like '%Model of%' "+
-    " ) p "+
-"inner join(select top 100000 * from TOTAL) t "+
-  " on t.hppty = p.HPROP "+
-"inner join ACCT a "+
-
-    "on t.hacct = a.hmy "+
-"inner join UL_BOOKS b "+
-
-    "on t.ibook = b.book";
-
-                using (SqlCommand cmd = new SqlCommand(text, conn))
+                using (SqlCommand cmd = new SqlCommand ( text, conn ))
                 {
-                    SqlDataReader dr =cmd.ExecuteReader();
-                   while(dr.Read())
+                    SqlDataReader dr = cmd.ExecuteReader ();
+                    while (dr.Read ())
                     {
-                        log.LogInformation($" Function successfully read data: ", dr[0]);
+                        //log.LogInformation ( $" Function successfully read data: ", dr[0] );
                         //call blackline http and send data
                         try
                         {
-                            var url = Environment.GetEnvironmentVariable("blacklineUrlBase") + "accounts?message-version=5";
+                            var url = Environment.GetEnvironmentVariable ( "blacklineUrlBase" ) + "dataingestion/accounts?message-version=5";
                             // Convert all request param into Json object
 
-                            var content = dr[0].ToString();
-                                //"PRO-C	36010000	42	1102	BK	12111				Current Year Retained Earnings	Cash	A	         0	TRUE	TRUE	GBP	USD	3/31/2022	34664.86	23058.37	34664.86	IMPCash 2	IMPCash 3	IMPCash 4	IMPCash 5	IMPCash 6";
+                            var content = dr[0].ToString ();
+                            //"PRO-C	36010000	42	1102	BK	12111				Current Year Retained Earnings	Cash	A	         0	TRUE	TRUE	GBP	USD	3/31/2022	34664.86	23058.37	34664.86	IMPCash 2	IMPCash 3	IMPCash 4	IMPCash 5	IMPCash 6";
 
                             // Call Your  API
-                            HttpClient newClient = new HttpClient();
-                            HttpRequestMessage newRequest = new HttpRequestMessage(HttpMethod.Put, string.Format(url));
-                            newRequest.Headers.Add("Authorization", "Bearer " +
-                                "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkNINGwzQ0JqUDQ0R1QtSXJsVGNLbWxfc1hKTSIsImtpZCI6IkNINGwzQ0JqUDQ0R1QtSXJsVGNLbWxfc1hKTSJ9.eyJleHAiOjE2NDk3MDY5OTYsImlzcyI6Imh0dHBzOi8vc2J1cy5hcGkuYmxhY2tsaW5lLmNvbSIsImF1ZCI6Imh0dHBzOi8vc2J1cy5hcGkuYmxhY2tsaW5lLmNvbS9yZXNvdXJjZXMiLCJjbGllbnRfaWQiOiJsaW5rbG9naXN0aWNzX3N0czIiLCJzY29wZSI6WyJEYXRhSW5nZXN0aW9uQVBJIiwiaW5zdGFuY2VfNkU5MkYzNEItQzA2Ri00OUVELUFGNzEtQ0E5MDZFQTE3NTVCIl0sInN1YiI6IjMxNDA2NzhDLUQ5Q0ItNEFGMC05NUM2LUQ0NEZFRkREMkE4QSIsImF1dGhfdGltZSI6MTY0OTcwMzM5NiwiaWRwIjoiaWRzcnYiLCJsYW5ndWFnZSI6ImVuLVVTLGVuO3E9MC45In0.Ykrcsb3Qqp60XtjKrkQNbCDCY1W4PzpIs1bMEUlwk6CsoQKVwuQBQND3lRldsT58NgyRx3T_KpO2nFZ2cnk5cr_TU-Da3a3xep3CpyCrwYdIz_BcG7vDlhpW0nBRa8wSBWpNvNWcqChxp4saru5m6Jk2EEt1IlkELahXANir8ZLKxdqXz3Alk2yrrWBWtBvJUO8Y9Hf6YpOswvhOXnKhbtzi7aEtGdgG9A_V7Xbev6bx0V1OZKJunGKN69IAZsnoFoCzfBcOA28HJFIwGWoOu1635YO2LSTyB7Gvwb8HZ_zyM8FW6ZHnzETm9okw2mX-Njq2G4YkQ4nzQuYA8sTeYA");
-                            newRequest.Content = new StringContent(content, Encoding.UTF8, "application/json");
+                            HttpRequestMessage newRequest = new HttpRequestMessage ( HttpMethod.Put, string.Format ( url ) );
+                            newRequest.Headers.Add ( "Authorization", "Bearer " +
+                                token.AccessToken );
+                            newRequest.Content = new StringContent ( content, Encoding.UTF8, "application/json" );
                             //Read Server Response
-                            HttpResponseMessage response = await newClient.SendAsync(newRequest);
-                            log.LogInformation($" Function successfully inserterted: ", response);
+                            HttpResponseMessage response = await newClient.SendAsync ( newRequest );
+                            //if (response.StatusCode == HttpStatusCode.Accepted)
+                            //    log.LogInformation ( $" Function successfully inserterted: ", response.Content );
+                            //else
+                            //    log.LogInformation ( $" Function Failed in the Blackline API call: ", response.Content );
 
                         }
                         catch (Exception ex)
                         {
-                            log.LogInformation($" Exception occured in function executed at: {DateTime.Now} ", ex.Message);
-
+                            log.LogInformation ( $" Exception occured in function executed at: {DateTime.Now} ", ex.Message );
+                            //Log it to a table for reporting
+                            //Incremental load
+                            //Alerts and notification
+                        }
+                        finally
+                        {
+                           
                         }
                     }
                 }
             }
 
-
+            log.LogInformation ( $"C# Timer trigger function Ent at: {DateTime.Now}" );
             //------------------------
-           
+
         }
-        //log
+
 
     }
-    
+
+    internal class Token
+    {
+        [JsonProperty ( "access_token" )]
+        public string AccessToken { get; set; }
+
+        [JsonProperty ( "token_type" )]
+        public string TokenType { get; set; }
+
+        [JsonProperty ( "expires_in" )]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty ( "refresh_token" )]
+        public string RefreshToken { get; set; }
+    }
+
 
 }
